c56e480a56367f8926d82c60284649d2e47a1333
WillHeather18 <s5418522@bournemouth.ac.uk>
2024-05-14 15:26:10 +0100

    14/05/2024

--- lib/service/recipe_service.dart
+++ lib/service/recipe_service.dart
@@ -3,6 +3,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class RecipeService {
   final CollectionReference recipes =
       FirebaseFirestore.instance.collection('recipes');
+  final CollectionReference users =
+      FirebaseFirestore.instance.collection('users');
 
   // Add a new recipe
   Future<void> addRecipe(Map<String, dynamic> recipeData) async {
@@ -34,5 +36,54 @@ class RecipeService {
         .delete()
         .then((value) => print("Recipe Deleted"))
         .catchError((error) => print("Failed to delete recipe: $error"));
+  }
+
+  // Add a like to a recipe
+  Future<void> addLike(
+    String recipeId,
+    String username,
+  ) async {
+    QuerySnapshot userQuery =
+        await users.where('username', isEqualTo: username).get();
+
+    if (userQuery.docs.isEmpty) {
+      print("No user found with username: $username");
+      return;
+    }
+
+    DocumentSnapshot userDoc = userQuery.docs[0];
+    List<dynamic> likes =
+        (userDoc.data() as Map<String, dynamic>)['likes'] ?? [];
+
+    bool alreadyLiked = likes.contains(recipeId);
+    if (alreadyLiked) {
+      // Unlike the post
+      likes.remove(recipeId);
+      await userDoc.reference.update({'likes': likes});
+
+      QuerySnapshot recipeQuery =
+          await recipes.where('recipeId', isEqualTo: recipeId).get();
+      if (recipeQuery.docs.isNotEmpty) {
+        await recipeQuery.docs[0].reference
+            .update({'interactions.likes': FieldValue.increment(-1)});
+        print("Like Removed");
+      } else {
+        print("No document found with recipeId: $recipeId");
+      }
+    } else {
+      // Like the post
+      likes.add(recipeId);
+      await userDoc.reference.update({'likes': likes});
+
+      QuerySnapshot recipeQuery =
+          await recipes.where('recipeId', isEqualTo: recipeId).get();
+      if (recipeQuery.docs.isNotEmpty) {
+        await recipeQuery.docs[0].reference
+            .update({'interactions.likes': FieldValue.increment(1)});
+        print("Like Added");
+      } else {
+        print("No document found with recipeId: $recipeId");
+      }
+    }
   }
 }
--- lib/widgets/recipe_card.dart
+++ lib/widgets/recipe_card.dart
@@ -1,23 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:timeago/timeago.dart' as timeago;
 import '../pages/recipe_page.dart';
+import '../service/recipe_service.dart';
+import '../service/user_service.dart';
+import '../providers/user_provider.dart';
+import 'package:provider/provider.dart';
 
 class RecipeCard extends StatefulWidget {
   final Map<String, dynamic> recipeData;
+  final String username;
 
-  RecipeCard({required this.recipeData});
+  RecipeCard({required this.recipeData, required this.username});
 
   @override
   State<RecipeCard> createState() => _RecipeCardState();
 }
 
 class _RecipeCardState extends State<RecipeCard> {
+  var recipeService = RecipeService();
+  var userService = UserService(
+    userProvider: UserProvider(),
+  );
   bool isLiked = false;
 
   bool showMore = false;
 
   @override
   Widget build(BuildContext context) {
+    print("Username: ${widget.username}");
+
+    var isLiked = userService.checkIsLiked(
+        widget.recipeData['recipeId'], widget.username);
+
     return Container(
       decoration: const BoxDecoration(),
       child: Padding(
@@ -34,9 +48,35 @@ class _RecipeCardState extends State<RecipeCard> {
               ),
               child: Row(
                 children: [
-                  Text(widget.recipeData['author']['username'],
-                      style: const TextStyle(
-                          fontWeight: FontWeight.bold, fontSize: 24)),
+                  Icon(Icons.account_circle_outlined,
+                      size: 50, color: Colors.grey[700]),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8.0),
+                    child: Text(widget.recipeData['author']['username'],
+                        style: const TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 24)),
+                  ),
+                  const Expanded(
+                    child: SizedBox(),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 8.0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Handle follow button press
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blue, // background color
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(24.0),
+                        ),
+                      ),
+                      child: const Text(
+                        'Follow',
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ),
@@ -49,75 +89,162 @@ class _RecipeCardState extends State<RecipeCard> {
                             RecipeDetailsPage(recipeData: widget.recipeData)),
                   );
                 },
-                child: Stack(
-                  children: [
-                    AspectRatio(
-                      aspectRatio: 1 / 1,
-                      child: Container(
-                        decoration: const BoxDecoration(),
-                        child: Hero(
-                          tag: widget.recipeData['imageURL'],
-                          child: Image.network(
-                            widget.recipeData['imageURL'],
-                            fit: BoxFit.cover,
-                            width: double.infinity,
+                child: Container(
+                  width: double.infinity,
+                  child: AspectRatio(
+                    aspectRatio: 1 / 1,
+                    child: Stack(
+                      children: [
+                        AspectRatio(
+                          aspectRatio: 1 / 1,
+                          child: Container(
+                            decoration: const BoxDecoration(),
+                            child: Hero(
+                              tag: widget.recipeData['imageURL'],
+                              child: Image.network(
+                                widget.recipeData['imageURL'],
+                                fit: BoxFit.cover,
+                                width: double.infinity,
+                              ),
+                            ),
                           ),
                         ),
-                      ),
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 20, left: 5),
+                          child: Align(
+                            alignment: Alignment.bottomLeft,
+                            child: Container(
+                                height: 20,
+                                width: MediaQuery.of(context)
+                                    .size
+                                    .width, // Set a width for the Container
+                                decoration: const BoxDecoration(
+                                  color: Color(0x0000cd77),
+                                  borderRadius: BorderRadius.only(
+                                    topRight: Radius.circular(10),
+                                  ),
+                                ),
+                                child: Wrap(
+                                  spacing: 8.0, // gap between adjacent tags
+                                  runSpacing: 4.0, // gap between lines
+                                  direction:
+                                      Axis.horizontal, // direction of the Wrap
+                                  alignment: WrapAlignment
+                                      .start, // align tags to the end
+                                  children: List.generate(
+                                    widget.recipeData['tags'].length,
+                                    (index) {
+                                      // List of colors
+                                      List<Color> colors = [
+                                        Colors.red,
+                                        Colors.green,
+                                        Colors.blue,
+                                        Colors.yellow,
+                                        Colors.orange,
+                                        // Add more colors if needed
+                                      ];
+
+                                      return Container(
+                                        height: 32,
+                                        decoration: BoxDecoration(
+                                          color: colors[index %
+                                              colors
+                                                  .length], // Select a color based on the index
+                                          borderRadius: const BorderRadius.all(
+                                              Radius.circular(10)),
+                                        ),
+                                        child: Padding(
+                                          padding: const EdgeInsets.all(2.0),
+                                          child: Text(
+                                            widget.recipeData['tags'][index],
+                                            style: const TextStyle(
+                                              fontSize: 24,
+                                              color: Colors.white,
+                                            ),
+                                          ),
+                                        ),
+                                      );
+                                    },
+                                  ),
+                                )),
+                          ),
+                        )
+                      ],
                     ),
-                    Align(
-                      alignment: Alignment.bottomRight,
-                      child: Container(
-                        height: 20,
-                        decoration: const BoxDecoration(
-                          color: Color(0x0000cd77),
-                          borderRadius: BorderRadius.only(
-                            topRight: Radius.circular(10),
-                          ),
-                        ),
-                        child: ListView.builder(
-                          itemCount: widget.recipeData['tags'].length,
-                          itemBuilder: (context, index) {
-                            return Container(
-                              height: 20,
-                              width: 60,
-                              decoration: const BoxDecoration(
-                                color: Color.fromARGB(255, 12, 179, 109),
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(10)),
-                              ),
-                              child: Text(
-                                widget.recipeData['tags'][index],
-                                style: const TextStyle(color: Colors.white),
-                              ),
-                            );
-                          },
-                        ),
-                      ),
-                    )
-                  ],
+                  ),
                 )),
             Container(
               height: 50,
               decoration: const BoxDecoration(),
               child: Row(
                 children: [
-                  IconButton(
-                    icon: const Icon(Icons.favorite_border),
-                    onPressed: () {},
+                  Row(
+                    children: [
+                      FutureBuilder<bool>(
+                        future: isLiked,
+                        builder: (BuildContext context,
+                            AsyncSnapshot<bool> snapshot) {
+                          if (snapshot.connectionState ==
+                              ConnectionState.waiting) {
+                            return IconButton(
+                              icon: const Icon(Icons.favorite_border),
+                              onPressed: () {},
+                            );
+                          } else if (snapshot.hasError) {
+                            return Text('Error: ${snapshot.error}');
+                          } else {
+                            return IconButton(
+                              icon: Icon(
+                                snapshot.data ?? false
+                                    ? Icons.favorite
+                                    : Icons.favorite_border,
+                                color:
+                                    snapshot.data ?? false ? Colors.red : null,
+                              ),
+                              onPressed: () {
+                                recipeService.addLike(
+                                    widget.recipeData['recipeId'],
+                                    widget.username);
+                              },
+                            );
+                          }
+                        },
+                      ),
+                      Text(
+                        widget.recipeData['interactions']['likes'].toString(),
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ],
                   ),
-                  IconButton(
-                    icon: const Icon(Icons.comment_outlined),
-                    onPressed: () {},
+                  Row(
+                    children: [
+                      IconButton(
+                        icon: const Icon(Icons.comment_outlined),
+                        onPressed: () {},
+                      ),
+                      Text(
+                        widget.recipeData['interactions']['comments'].length
+                            .toString(),
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ],
                   ),
-                  IconButton(
-                    icon: const Icon(Icons.share_outlined),
-                    onPressed: () {},
+                  Row(
+                    children: [
+                      IconButton(
+                        icon: const Icon(Icons.share_outlined),
+                        onPressed: () {},
+                      ),
+                      Text(
+                        widget.recipeData['interactions']['shares'].toString(),
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ],
                   ),
                 ],
               ),
             ),
-            Row(),
+            const Row(),
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Text(
